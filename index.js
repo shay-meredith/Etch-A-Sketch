233c3b6d1a11e6b91cec6c3a29ce54877721a59a
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-16 18:58:05 -0500

    Add partial click behavior

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function createGrid() {
-    let size = document.getElementById("gridSize").value;
-    const div = document.createElement("DIV");
+  let size = document.getElementById("gridSize").value;
+  console.log("The size is " + size);
+  document.getElementById("sketchContainer").style = `grid-template: repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+  for (i = 0; i < size * size; i++) {
+    addDiv();
+    document.getElementById("sketchContainer").childNodes[i].addEventListener("click", function() {
+        this.className = "clicked";
+      });
+  }
+}
 
-    document.getElementById("sketchContainer").style = `grid-template: repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
-    console.log(size);
-    /*
-    for (i = 0; i < size; i++){
-        document.getElementById("sketchContainer").appendChild(div);
-    }
-    */
+function addDiv() {
+  document.getElementById("sketchContainer").appendChild(document.createElement("DIV"));
 }
