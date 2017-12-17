5ebf7ff6ec862d36322709dfae93dc63f0778bb8
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-16 19:04:13 -0500

    Add toggle to click behavior

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function createGrid() {
   let size = document.getElementById("gridSize").value;
-  console.log("The size is " + size);
   document.getElementById("sketchContainer").style = `grid-template: repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
   for (i = 0; i < size * size; i++) {
     addDiv();
     document.getElementById("sketchContainer").childNodes[i].addEventListener("click", function() {
-        this.className = "clicked";
+        this.classList.toggle("clicked");
       });
   }
 }
