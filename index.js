3ef9d5034f12ebd1f98a5dab561081053ca33a96
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-19 08:14:55 -0500

    Add correct click and hover behavior

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sketchContainer = document.getElementById("sketchContainer");
 
+let size = document.getElementById("gridSize").value;
+
 const bodyElement = document.body;
 bodyElement.onload = createGrid;
 
@@ -15,21 +17,23 @@ const hoverContainer = document.getElementsByClassName("container")[1];
 const hoverCheckmark = document.getElementById("hover");
 const dragContainer = document.getElementsByClassName("container")[2];
 const dragCheckmark = document.getElementById("drag");
-clickContainer.onmouseup = checkmarkClicked;
-hoverContainer.onmouseup = checkmarkClicked;
-dragContainer.onmouseup = checkmarkClicked;
+
+//TODO: Change these when the next todo is fixed
+clickContainer.onmouseup = radioChecked;
+hoverContainer.onmouseup = radioChecked;
+dragContainer.onmouseup = radioChecked;
 
 function createGrid() {
-    while (sketchContainer.hasChildNodes()) {
-        sketchContainer.removeChild(sketchContainer.firstChild);
-    }
-  let size = document.getElementById("gridSize").value;
+  while (sketchContainer.hasChildNodes()) {
+    sketchContainer.removeChild(sketchContainer.firstChild);
+  }
+  //let size = document.getElementById("gridSize").value;
   sketchContainer.style = `grid-template: repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
   for (i = 0; i < size * size; i++) {
     addDiv();
-    sketchContainer.childNodes[i].addEventListener("click", function() {
-        this.classList.toggle("clicked");
-    });
+    ///sketchContainer.childNodes[i].addEventListener("click", function() {
+      ///this.classList.toggle("clicked");
+    ///});
   }
 }
 
@@ -38,49 +42,78 @@ function addDiv() {
 }
 
 function clearGrid() {
-    let gridCount = sketchContainer.children.length;
-    for (i = 0; i < gridCount; i++) {
-        sketchContainer.childNodes[i].classList.remove("clicked");
-    }
+  let gridCount = sketchContainer.children.length;
+  for (i = 0; i < gridCount; i++) {
+    sketchContainer.childNodes[i].classList.remove("clicked");
+  }
 }
 
-function checkmarkClicked() {
-    if (this.childNodes[3].childElementCount < 1) {
-        clickCheckmark.style.backgroundColor = "#eee";
-        if (clickCheckmark.childElementCount > 0) {
-            clickCheckmark.removeChild(clickCheckmark.childNodes[0]);
-        }
-        hoverCheckmark.style.backgroundColor = "#eee";
-        if (hoverCheckmark.childElementCount > 0) {
-            hoverCheckmark.removeChild(hoverCheckmark.childNodes[0]);
-        }
-        dragCheckmark.style.backgroundColor = "#eee";
-        if (dragCheckmark.childElementCount > 0) {
-            dragCheckmark.removeChild(dragCheckmark.childNodes[0]);
-        }
+function radioChecked() {
+  if (this.childNodes[3].childElementCount < 1) {
+    clickCheckmark.style.backgroundColor = "#eee";
+    if (clickCheckmark.childElementCount > 0) {
+      clickCheckmark.removeChild(clickCheckmark.childNodes[0]);
+    }
+    hoverCheckmark.style.backgroundColor = "#eee";
+    if (hoverCheckmark.childElementCount > 0) {
+      hoverCheckmark.removeChild(hoverCheckmark.childNodes[0]);
+    }
+    dragCheckmark.style.backgroundColor = "#eee";
+    if (dragCheckmark.childElementCount > 0) {
+      dragCheckmark.removeChild(dragCheckmark.childNodes[0]);
+    }
 
-        this.childNodes[3].style.backgroundColor = "#9a0007";
-        const span = document.createElement("SPAN");
-        this.childNodes[3].appendChild(span);
-        this.childNodes[3].children[0].className = "checkedContainer"
+    this.childNodes[3].style.backgroundColor = "#9a0007";
+    const span = document.createElement("SPAN");
+    this.childNodes[3].appendChild(span);
+    this.childNodes[3].children[0].className = "checkedContainer";
+
+    drawType(this);
+  } else {
+    this.childNodes[3].style.backgroundColor = "#eee";
+    this.childNodes[3].removeChild(this.childNodes[3].childNodes[0]);
+  }
+}
+
+function drawType(radioContainer) {
+    if (radioContainer === clickContainer) {
+        for (i = 0; i < size * size; i++) {
+            sketchContainer.childNodes[i].removeEventListener("mouseover", hovered);
+            sketchContainer.childNodes[i].addEventListener("click", clicked);
+        }
+    }
+    else if (radioContainer === hoverContainer) {
+        for (i = 0; i < size * size; i++) {
+            sketchContainer.childNodes[i].removeEventListener("click", clicked);
+            sketchContainer.childNodes[i].addEventListener("mouseover", hovered);
+        }
     } else {
-        this.childNodes[3].style.backgroundColor = "#eee";
-        this.childNodes[3].removeChild(this.childNodes[3].childNodes[0]);
+        console.log("Drag");
     }
 }
+
+function clicked() {
+    this.classList.toggle("clicked");
+}
+
+function hovered() {
+    this.classList.add("clicked");
+}
+
 
 //TODO: Finish radio select function (completely untested)
 function selectDrawType() {
-    if (document.getElementById("click").checked == true) {
-        sketchContainer.childNodes[i].addEventListener("click", function() {
-        this.classList.toggle("clicked")});
-    }
-    else if (document.getElementById("hover").checked == true) {
-        sketchContainer.childNodes[i].addEventListener("hover", function() {
-        this.classList.toggle("clicked")});
-    }
-    else {
-        sketchContainer.childNodes[i].addEventListener("mousedown", function() {
-        this.classList.toggle("clicked")});
-    }
+  if (document.getElementById("click").checked == true) {
+    sketchContainer.childNodes[i].addEventListener("click", function() {
+      this.classList.toggle("clicked");
+    });
+  } else if (document.getElementById("hover").checked == true) {
+    sketchContainer.childNodes[i].addEventListener("hover", function() {
+      this.classList.toggle("clicked");
+    });
+  } else {
+    sketchContainer.childNodes[i].addEventListener("mousedown", function() {
+      this.classList.toggle("clicked");
+    });
+  }
 }
